39cab37b6a954efd960154acf78d4a18cb25479c
alex <Zinaida.d@2muchcoffee.com>
2019-09-10 19:02:36 +0300

    feat(components:Customers:Customers)implemented Customers component

---
+++ src/components/Customers/Customers.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { Table, TableBody, TableHead, TableRow, Paper, Container } from '@material-ui/core';
+import StyledTableCell from '../../shared/components/Table/StyledTableCell';
+// import StyledTableRow from '../../shared/components/Table/StyledTableRow';
+import { styles } from '../../shared/components/styles/styles';
+
+// import { connect, useSelector } from 'react-redux';
+// import { bindActionCreators, Dispatch } from 'redux';
+// import { getCustomers } from '../../store/customers/actions';
+
+// import { getCustomersArray } from '../../store/customers/selectors';
+
+
+// const mapDispatchToProps = (dispatch: Dispatch) =>
+//   bindActionCreators({
+//     getCustomers
+//   }, dispatch);
+
+// type Props = ReturnType<typeof mapDispatchToProps>;
+
+const Customers: React.FC = ({...props}) => {
+
+  // useEffect(() => {
+  //   getCustomers();
+  // }, [getCustomers]);
+  //
+  // const customers = useSelector(state => getCustomersArray(state))
+
+  // const customersRows = customers.map(customer => (
+  //   <StyledTableRow key={customer._id}>
+  //     <StyledTableCell component="th" scope="row">{customer.name}</StyledTableCell>
+  //     <StyledTableCell>{customer.address}</StyledTableCell>
+  //     <StyledTableCell>{customer.phone}</StyledTableCell>
+  //   </StyledTableRow>
+  // ))
+
+  return (
+    <Container>
+      <Paper style={styles.root}>
+        <Table style={styles.table}>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Customer Name</StyledTableCell>
+              <StyledTableCell>Customer Address</StyledTableCell>
+              <StyledTableCell>Customer Phone Number</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+
+            {/*{customersRows}*/}
+
+          </TableBody>
+        </Table>
+      </Paper>
+    </Container>
+  );
+}
+
+export default Customers
+
+
+
+// export default connect(null, mapDispatchToProps)(Customers);
