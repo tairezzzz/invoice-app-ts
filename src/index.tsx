71b4066cf38709dfb85f538a2dcc051c71d352aa
alex <Zinaida.d@2muchcoffee.com>
2019-09-10 18:52:57 +0300

    feat(src:index) implemented custom theme for material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from '@material-ui/styles';
+
+
+
+
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#b2dfdb',
+    },
+    secondary: {
+      light: '#d1c4e9',
+      main: '#b39ddb',
+      contrastText: '#e3f2fd',
+    },
+    // red: {
+    //   main: '#ef5350'
+    // },
+  },
+});
+
+const app = (
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
