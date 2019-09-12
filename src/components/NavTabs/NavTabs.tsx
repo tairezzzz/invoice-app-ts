8c220f3c9a7168fe770549174f7e0daed407d06b
alex <Zinaida.d@2muchcoffee.com>
2019-09-12 14:34:34 +0300

    feat(components:Navtabs:NavTabs) implemented displaying amount of active invoices

--- src/components/NavTabs/NavTabs.tsx
+++ src/components/NavTabs/NavTabs.tsx
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { Link } from 'react-router-dom';
+import { getInvoicesArray } from '../../store/invoices/selectors';
+import { Actions as InvoicesActions } from '../../store/invoices/actions';
+import { RootState } from '../../store/index';
 
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
-
-import { Link } from 'react-router-dom';
-// import { connect, useSelector } from 'react-redux';
-// import { getInvoicesArray } from '../../store/invoices/selectors';
 import { styles } from './styles'
-// import { bindActionCreators } from 'redux';
-// import { getInvoices } from '../../store/invoices/actions';
 
 
-// const mapDispatchToProps = dispatch =>
-//     bindActionCreators({
-//         getInvoices,
-//     }, dispatch);
 
 
-const NavTabs: React.FC = () => {
 
-    // useEffect(() => {
-    //     getInvoices();
-    // }, [getInvoices]);
 
-    // const invoices = useSelector(state => getInvoicesArray(state))
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+    getInvoices: () => dispatch(InvoicesActions.getInvoices()),
+});
 
-    // const invoicesAmount = invoices.length
+type Props = ReturnType<typeof mapDispatchToProps>;
+
+
+const NavTabs: React.FC<Props> = ({getInvoices}) => {
+
+    useEffect(() => {
+        getInvoices();
+    }, [getInvoices]);
+
+    const invoices = useSelector((state: RootState) => getInvoicesArray(state))
+
+
+    const invoicesAmount = invoices.length
 
     return (
         <div>
@@ -32,20 +38,17 @@ const NavTabs: React.FC = () => {
                 <Container>
                     <Toolbar variant="dense" style={styles.toolbar}>
                         <Link to="/">
-                            <Typography variant="h6" color="textPrimary"> Logo</Typography>
+                            <Typography variant="h6" color="textPrimary">Logo</Typography>
                         </Link>
                         <Link to="/products">
-                            <Typography variant="h6" color="textPrimary"> Products</Typography>
+                            <Typography variant="h6" color="textPrimary">Products</Typography>
                         </Link>
                         <Link to="/customers">
-                            <Typography variant="h6" color="textPrimary"> Customers</Typography>
+                            <Typography variant="h6" color="textPrimary">Customers</Typography>
                         </Link>
                         <Link to="/invoices">
-                            <Typography variant="h6" color="textPrimary"> Invoices (
-                                {/*{invoicesAmount}*/}
-                                )</Typography>
+                            <Typography variant="h6" color="textPrimary">Invoices ({invoicesAmount})</Typography>
                         </Link>
-
                     </Toolbar>
                 </Container>
             </AppBar>
@@ -54,7 +57,5 @@ const NavTabs: React.FC = () => {
     );
 }
 
-export default NavTabs;
 
-
-// export default connect(null, mapDispatchToProps)(withRouter(NavTabs));
+export default connect(null, mapDispatchToProps)(NavTabs);
