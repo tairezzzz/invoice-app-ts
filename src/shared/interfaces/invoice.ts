8119b40922bafe571332765915d81641166e2eec
alex <Zinaida.d@2muchcoffee.com>
2019-09-13 17:38:24 +0300

    upd(shared:interface:entities) added types for entities and for InvoiceInput & InvoiceItemInput(shared:interface:invoice)

--- src/shared/interfaces/invoice.ts
+++ src/shared/interfaces/invoice.ts
@@ -5,9 +5,23 @@ export interface Invoice {
   total: number;
 }
 
+export interface InvoiceInput {
+  _id?: string;
+  customer_id: string;
+  discount: number;
+  total: number;
+}
+
 export interface InvoiceItem {
   _id: string;
   invoice_id: string;
   product_id: string;
   quantity: number;
+}
+
+export interface InvoiceItemInput {
+  _id: string;
+  product_id: string;
+  quantity: number;
+  invoice_id?: string;
 }
