a0321a7195ff2e6dd612ffb0a9e2fc85c188fce0
alex <Zinaida.d@2muchcoffee.com>
2019-09-13 19:22:48 +0300

    upd(Rooter) connected Create & Edit Invoice to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,15 +3,21 @@ import { Route } from 'react-router-dom';
 import Customers from './components/Customers/Customers';
 import Products from './components/Products/Products';
 import Invoices from './components/Invoices/Invoices';
+import CreateInvoice from './components/Invoices/CreateInvoice/CreateInvoice';
+import EditInvoice from './components/Invoices/EditInvoice/EditInvoice';
+
+
 
 
 const Router: React.FC = () => {
     return (
       <>
-          <Route exact path='/' component={Invoices} />
-          <Route exact path='/customers' component={Customers} />
+        <Route exact path='/' component={Invoices} />
+        <Route exact path='/customers' component={Customers} />
         <Route exact path='/products' component={Products} />
         <Route exact path='/invoices' component={Invoices} />
+        <Route exact path='/invoice/new' component={CreateInvoice} />
+        <Route exact path='/invoice/:id/edit' component={EditInvoice} />
       </>
     );
 }
