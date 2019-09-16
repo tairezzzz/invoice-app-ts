0b7441c2deccd2f859e0a133c9d2b8164e22dd30
alex <Zinaida.d@2muchcoffee.com>
2019-09-16 18:08:06 +0300

    feat(components:Invoices:CreateInvoice) added redirecting to invoices page after creating

--- src/components/Invoices/CreateInvoice/CreateInvoice.tsx
+++ src/components/Invoices/CreateInvoice/CreateInvoice.tsx
@@ -4,25 +4,26 @@ import { Dispatch } from 'redux';
 import { Actions as InvoicesActions } from '../../../store/invoices/actions';
 import { InvoiceInput} from '../../../shared/interfaces/invoice';
 import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   postInvoice: (payload: InvoiceInput) => dispatch(InvoicesActions.postInvoice(payload)),
-
 });
 
-type Props = ReturnType<typeof mapDispatchToProps>;
+type Props = ReturnType<typeof mapDispatchToProps> & RouteComponentProps;
 
-const CreateInvoice: React.FC<Props> = ({postInvoice}) => {
+const CreateInvoice: React.FC<Props> = ({postInvoice, history}) => {
 
   const onSubmit = (payload: InvoiceInput) => {
     postInvoice(payload)
+    history.push("/invoices")
   }
 
   return (
     <InvoiceForm
       buttonText={"save invoice"}
       onSubmit={onSubmit}
-
     />
   )
 }
