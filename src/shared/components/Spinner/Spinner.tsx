6d2064bc67d8be201873e4ca4391583b7b81404b
alex <Zinaida.d@2muchcoffee.com>
2019-09-16 18:47:53 +0300

    feat(shared:components:Spinner) changed circular spinner to linear

--- src/shared/components/Spinner/Spinner.tsx
+++ src/shared/components/Spinner/Spinner.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles(theme => ({
   progress: {
-    margin: theme.spacing(2),
+    margin: theme.spacing(8),
   },
 }));
 
@@ -13,7 +13,7 @@ export default function Spinner() {
 
   return (
     <>
-      <CircularProgress className={classes.progress} color="secondary" />
+      <LinearProgress className={classes.progress} color="secondary" />
     </>
   );
 }
